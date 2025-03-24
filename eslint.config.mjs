43833418e10663bde651f3bc6a6feaa9c7f3adbc
Jordy Sarmiento <jordysarmiento@gmail.com>
2025-03-23 21:02:41 -0500

    [feat] EsLint Action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,23 +13,28 @@ export default tseslint.config(
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
+      parser: tseslint.parser,
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: "latest",
       sourceType: 'module',
       parserOptions: {
-        projectService: true,
+        project: ["tsconfig.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
     },
   },
 );
